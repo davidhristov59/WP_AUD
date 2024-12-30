5f004c4db1ba6cf343a67a08510dae8cbc4f65e3
David Hristov <113634059+davidhristov59@users.noreply.github.com>
2024-12-30 18:56:21 +0100

    Initial commit

---
+++ src/main/java/mk/ukim/finki/wp_aud/WpAudApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.wp_aud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpAudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WpAudApplication.class, args);
+	}
+
+}
